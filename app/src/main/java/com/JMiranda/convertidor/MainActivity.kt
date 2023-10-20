09ea8a38ddacd954562ed0a7cbb12cbb7ad2bd00
Juan Antonio Miranda Arreola <a22311085@uthermosillo.edu.mx>
2023-10-20 08:56:54 -0700

    Se creo el diseño del convertidor y se agregaron los datos al spinner, Tambien se crearon las variables de las temperaturas

--- app/src/main/java/com/JMiranda/convertidor/MainActivity.kt
+++ app/src/main/java/com/JMiranda/convertidor/MainActivity.kt
@@ -2,10 +2,38 @@ package com.JMiranda.convertidor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var selectedTemp = findViewById<Spinner>(R.id.Convert_Options)
+        var temp = findViewById<EditText>(R.id.Temperature)
+        var Button = findViewById<Button>(R.id.Convert_Button)
+        var result = findViewById<TextView>(R.id.Temperature_Final)
+
+        if(selectedTemp != null){
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                resources.getStringArray(R.array.Convert_Options)
+            )
+        }
+
+        Button.setOnClickListener {
+            temp.text
+        }
+
+    }
+    fun convert(){
+        //Crear las conersiones
+        //tarea: Traer las formulas de conversion
+
     }
 }
---
+++ app/src/main/java/com/JMiranda/convertidor/models/Temperatura.kt
@@ -0,0 +1,8 @@
+package com.JMiranda.convertidor.models
+
+data class Temperatura(
+    val temperature: Double,
+    val origin: Char,
+    val destiny: Char
+
+)
